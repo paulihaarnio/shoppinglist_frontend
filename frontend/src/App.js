b93b5a7065afad9ce045836d01545d2115e62225
paulihaarnio <pauli.haarnio@gmail.com>
2022-03-01 13:10:05 +0200

    jotain tehty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import {useState, useEffect} from 'react'
 import './App.css';
+const URL ='http://localhost/shoppinglist_backend/'
+
 
 function App() {
+  const [item, setItem] = useState('')
+  const [amount, setAmount] = useState('')
+  const [items, setItems] = useState([])
+  useEffect(() => {
+  axios.get(URL).then((response)=>{
+    console.log(response.data);
+    setItems(response.data)
+  }).catch(error=>{
+    alert(error.response ? error.response.data.error:error)
+  })
+  }, [])
+
+  function save(e){
+    e.preventDefault()
+    const json=JSON.stringify(item)
+    axios.post(URL+'add.php',json,{
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then((response)=>{
+      setItems(items=>[...items,response.data])
+      setItem('')
+      setAmount(amount)
+    }).catch(error=>{
+      alert(error.response ? error.response.data.error:error)
+    })
+  }
+
+function remove(id){
+const json =JSON.stringify({id:id})
+axios.post(URL+'delete.php',json,{
+  headers:{'Content-Type':'application/json'}
+})
+.then((response)=>{
+  const newlistwithoutremoved= items.filter((item)=>item.id!==id);
+  setItems(newlistwithoutremoved);
+}).catch(error=>{
+  alert(error.response ? error.response.data.error : error)
+})
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+        <h2>Shopping list Pauli Haarnio</h2>
+        <label>New item</label>
+        <input value={item.description} placeholder='Add a new item' onChange={e=>setItem(e.target.value)}></input>
+        <input value={item.amount} placeholder='Add amount' onChange={e=>setAmount(e.target.value)}></input>
+        <button>Add</button>
+      </form>
+      <ol>
+        {items?.map(item=>(
+          <li key={item.id}>
+            {item.description}&nbsp;
+            {item.amount}&nbsp;
+          <a href='#' className="delete" onClick={()=>remove(item.id)}>
+            Delete
+          </a>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
